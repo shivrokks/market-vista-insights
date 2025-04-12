d636f5feb8d96c7655c417c6bfb2fa05d2c56568
swastik-3101 <swastikkr1908@gmail.com>
2025-04-12 15:06:47 +0530

    correcting github

--- backend/config/default.js
+++ backend/config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI: "mongodb+srv://meetrocks31:KvqNL7xMD2BAp4bd@stockhack.z5movr9.mongodb.net/",
-  finnhubApiKey: "YOUR_FINNHUB_API_KEY", // Replace with your actual Finnhub API key
+  finnhubApiKey: "cvt14b9r01qhup0tk7pgcvt14b9r01qhup0tk7q0", // Replace with your actual Finnhub API key
   alphaVantageApiKey: "4U7RRNDB7TJ78COU", // Replace with your actual Alpha Vantage API key
   jwtSecret: "your_jwt_secret_key", // In production, use environment variables
   jwtExpiration: "24h",
